7598e208e9cc1c04ce7ef06ecb81a5d52faac777
prabakaran202 <prabakaran20020430@gmail.com>
2025-10-08 07:26:51 +0530

    Add debug context processor to templates and update view rendering

--- my_project/praba/views.py
+++ my_project/praba/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 def Praba(request):
-   return HttpResponse.render(request, 'praba/intext.html')
+   
+   return render(request, 'praba/intex.html')
